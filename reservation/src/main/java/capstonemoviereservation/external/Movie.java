84cdf850273bb4732dcc95d58ed8cb561505e80d
adslhdsl <100065651+adslhdsl@users.noreply.github.com>
2023-03-06 14:29:00 +0900

    commit

---
+++ reservation/src/main/java/capstonemoviereservation/external/Movie.java
@@ -0,0 +1,17 @@
+package capstonemoviereservation.external;
+
+import lombok.Data;
+import java.util.Date;
+@Data
+public class Movie {
+
+    private Long movieId;
+    private String status;
+    private String desc;
+    private Long reviewCnt;
+    private String lastAction;
+    private Integer numberOfSeats;
+    private Long customerId;
+}
+
+
